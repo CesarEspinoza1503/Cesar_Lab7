379d6d5c043c388768ef03e3a3adc5b195ed93fc
Cesar <cesardespinoza@unitec.edu>
2016-12-02 17:38:18 -0600

    prueba

--- main.cpp
+++ main.cpp
@@ -15,32 +15,87 @@ void leerTxt(char*, vector<usuario*>*);
 int main(int argc, char const *argv[])
 {
 	ofstream archivo;
-	
-	vector<usuario*> admin;
+
+	vector<usuario*> admins;
 	vector<usuario*> interns;
 	vector<usuario*> supervisors;
 	vector<usuario*> managers;
-
+	string user_n, pass, correo;
 	int opt=1;
-	string user;
+	string use;
 	char* password;
+
+	leerTxt("Administrador.txt", &admins);
+	cout<<"TAMAÑO: "<<admins.size();
+	admins.at(0)->getCorreo();
 	do
 	{
 		cout<<"		LOG IN"<<endl<<"		---------------------"<<endl;
-		cout<<"		Username: ";
-		cin>> user;
-		cout<<"		Password: "
+		cout<<"		Username: "<<endl;
+		cin>> use;
+		cout<<"		Password: "<<endl;
 		cin >> password;
-		if(admin.at(0)->getUser() == user && admin.at(0)->getPass() == password)
+		if(admins.at(0)->getUser() == use && admins.at(0)->getPass() == password)
 		{
+			int admin_opt;
 
+			do
+			{
+				cout<<"1)Agregar un Manager \n2)Agregar un Interno"
+				<<"\n3)Agregar un Supervisor\n4)Eliminar un Manager"
+				<<"\n5)Eliminar un Interno\n6)Eliminar un Supervisor"
+				<<"\n7)Volver"<<endl;
+				cin>> admin_opt;
+				if (admin_opt == 1)
+				{
+					db_manager* manager = new db_manager();
+					string sueldo;
+					cout<<"		MANAGER"<<endl;
+					cout<<"Ingrese Nombre de usuario: "<<endl;
+					cin>>user_n;
+					cout<<"Ingrese contraseña: "<<endl;
+					cin>>pass;
+					cout<<"Ingrese correo: "<<endl;
+					cin>>correo;
+					cout<<"Ingrese Sueldo del Manager: "<<endl;
+					cin>>sueldo;
+					manager->setUser(user_n);
+					manager->setPass(pass);
+					manager->setCorreo(correo);
+					manager->setSueldo(sueldo);
+				}
+				if (admin_opt == 2)
+				{
+
+					string dias;
+					cout<<"		INTERNO"<<endl;
+					cout<<"Ingrese Nombre de usuario: "<<endl;
+					cin>>user_n;
+					cout<<"Ingrese contraseña: "<<endl;
+					cin>>pass;
+					cout<<"Ingrese correo: "<<endl;
+					cin>>correo;
+					cout<<"Ingrese Numero de dias que trabajara: "<<endl;
+					cin>>dias;
+				}
+				if (admin_opt == 3)
+				{
+
+					string sueldo;
+					cout<<"		SUPERVISOR"<<endl;
+					cout<<"Ingrese Nombre de usuario: "<<endl;
+					cin>>user_n;
+					cout<<"Ingrese contraseña: "<<endl;
+					cin>>pass;
+					cout<<"Ingrese correo: "<<endl;
+					cin>>correo;
+				}
+			} while (admin_opt!=7);
 		}
 		//db_administrador* adm = dynamic_cast<db_administrador*>()
-		leerTxt("Administrador.txt", &admin);
+		
+		cout<<admins.at(0)->getCorreo()<<endl;
 
-		cout<<admin.at(0)->getCorreo()<<endl;
-
-		opt=0;
 	} while (opt != 0);
 	return 0;
 }
@@ -48,7 +103,6 @@ int main(int argc, char const *argv[])
 
 
 void leerTxt(char* txt_name, vector<usuario*>* vec){
-	string a1 = " ";
 	string user, pass, correo, fecha;
 	string line;
 	ifstream archivo (txt_name);
@@ -57,7 +111,7 @@ void leerTxt(char* txt_name, vector<usuario*>* vec){
 		if (txt_name == "Administrador.txt")
 		{
 			db_administrador* admin = new db_administrador();
-			while( archivo >> user >> pass >> correo >>fecha)
+			while( archivo >> user >> pass >> correo >> fecha)
 			{
 				admin->setUser(user);
 				admin->setPass(pass);
@@ -68,6 +122,7 @@ void leerTxt(char* txt_name, vector<usuario*>* vec){
 		}
 		
 		archivo.close();
+
 	}else{
 		cout<< "El archivo no se pudo abrir.";
 	}
